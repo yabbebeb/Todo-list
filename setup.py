37eca075ea2d01e7bd5846b639b74866526c38fe
yabbebeb <152523028+yabbebeb@users.noreply.github.com>
2024-01-29 19:28:25 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{td1}',
+ version='1.0',
+ author='{yabbebeb}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
